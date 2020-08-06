35dba76d78b43fb950a34afdb491fd505e078aa7
Isabella Benabaye <isabellabenabaye@gmail.com>
2020-08-06 02:19:27 -0700

    :zap: added last else if + updated description

--- R/theme_ib.R
+++ R/theme_ib.R
@@ -3,10 +3,10 @@
 #' \code{theme_ib} is a [ggplot2] theme in Isabella Benabaye's personal style.
 #'
 #' By default (`plots_pane = FALSE`), the theme adjusts the text sizes for
-#' printing. `plots_pane = TRUE` is meant to be used when viewing plots in the
-#' plots pane and sizes are not adjusted.
-#'
-#' @import ggplot2
+#' printing.`plots_pane = TRUE` is meant to be used when viewing plots in the
+#' plots pane and text sizes are not adjusted.  There is also an option (`md =
+#' TRUE`) to use markdown theme elements from `ggtext` instead of
+#' `element_text()`.
 #'
 #' @md
 #' @param title_family title elements font family
@@ -17,6 +17,7 @@
 #' @param line_color color of line elements
 #' @param plot_margin plot margin specifications
 #' @param plots_pane indicate whether plots will be viewed in plots pane or not
+#' @param md indicate whether to use markdown elements for text
 #' @export
 
 theme_ib <- function(title_family = "IBM Plex Sans",
@@ -24,8 +25,8 @@ theme_ib <- function(title_family = "IBM Plex Sans",
                      base_size = 13, text_color = "gray20",
                      bg_color = "#F7F7F7", line_color = "gray50",
                      plot_margin = margin(20,20,20,20),
-                     md = FALSE,
-                     plots_pane = FALSE) {
+                     plots_pane = FALSE,
+                     md = FALSE) {
 
   if (plots_pane == FALSE & md == FALSE) {
     ggplot2::theme_minimal() +
@@ -80,6 +81,24 @@ theme_ib <- function(title_family = "IBM Plex Sans",
 
       legend.title = ggtext::element_markdown(size = base_size * 1.3),
       legend.text = element_text(size = base_size * 1.1)
+    )
+  } else if (plots_pane == TRUE & md == TRUE) {
+    ggplot2::theme_minimal(base_size = base_size) +
+    ggplot2::theme(
+      text = element_text(family = text_family,
+                          color = text_color),
+      title = element_text(family = title_family),
+      line = element_line(color = line_color),
+
+      plot.title = ggtext::element_markdown(face = "bold",
+                                lineheight = 1.2),
+      plot.title.position = "plot",
+      plot.subtitle = ggtext::element_markdown(lineheight = 1.2),
+      plot.margin = plot_margin,
+      plot.background = element_rect(fill = bg_color,
+                                     color = bg_color),
+
+      axis.title = ggtext::element_markdown(hjust = 1)
     )
   } else {
     ggplot2::theme_minimal(base_size = base_size) +
