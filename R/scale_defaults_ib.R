b7d80837fe99fedd0133de1ef1fafda6dd5550cc
Isabella Benabaye <isabellabenabaye@gmail.com>
2020-08-07 19:48:29 -0700

    :tada: scale default settings functions

---
+++ R/scale_defaults_ib.R
@@ -0,0 +1,35 @@
+#' Custom scale default settings
+#'
+#' Sets default values for the `expand` argument of scale_(x|y)_continuous and
+#' scale_(x|y)_discrete.
+#'
+#' @name scale_defaults_ib
+NULL
+
+#' @rdname scale_defaults_ib
+#' @export
+
+scale_x_c_ib <- function(...) {
+  ggplot2::scale_x_continuous(expand = expansion(add = 0.5), ...)
+}
+
+#' @rdname scale_defaults_ib
+#' @export
+
+scale_x_d_ib <- function(...) {
+  ggplot2::scale_x_discrete(expand = expansion(add = 0.5), ...)
+}
+
+#' @rdname scale_defaults_ib
+#' @export
+
+scale_y_c_ib <- function(...) {
+  ggplot2::scale_x_continuous(expand = expansion(mult = c(0, .05)), ...)
+}
+
+#' @rdname scale_defaults_ib
+#' @export
+
+scale_y_d_ib <- function(...) {
+  ggplot2::scale_x_discrete(expand = expansion(mult = c(0, .05)), ...)
+}
