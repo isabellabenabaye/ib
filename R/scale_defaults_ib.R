3501d6c7f89ede4bf7c8dc81ca09252b96160cf9
Isabella Benabaye <isabellabenabaye@gmail.com>
2020-08-12 01:10:46 -0700

    :pencil: scale_defaults_ib edit description

--- R/scale_defaults_ib.R
+++ R/scale_defaults_ib.R
@@ -1,7 +1,21 @@
 #' Custom scale default settings
 #'
-#' Sets default values for the `expand` argument of scale_(x|y)_continuous and
-#' scale_(x|y)_discrete.
+#' Sets default values for the `expand` argument of `scale_(x|y)_continuous` and
+#' `scale_(x|y)_discrete`.
+#'
+#' `scale_x_c_ib`: Sets default values for the `expand` argument of
+#' `scale_x_continuous` that adds 0.5 units of space on both sides of the plot
+#'
+#' `scale_x_d_ib`: Sets default values for the `expand` argument of
+#' `scale_x_discrete` that adds 0.5 units of space on both sides of the plot
+#'
+#' `scale_y_c_ib`: Sets default values for the `expand` argument of
+#' `scale_y_continuous` such that there is no space below the lowest value and
+#' the top end of the plot is extended by 5% (eg. for use with bar plots)
+#'
+#' `scale_y_d_ib`: Sets default values for the `expand` argument of
+#' `scale_y_discrete` such that there is no space below the lowest value and the
+#' top end of the plot is extended by 5% (eg. for use with bar plots)
 #'
 #' @name scale_defaults_ib
 NULL
